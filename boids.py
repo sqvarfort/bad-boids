e08b1ec46eb17091c32fec0fb90ea4903eca654a
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-02-23 13:20:03 +0000

    Merged file contents

--- boids.py
+++ boids.py
@@ -1,12 +1,89 @@
-# Boids
-#------------
-# Description: what we want here is a clas for Boids. Instead of just having a method, we want to construct a class that consists of an object 'the boid' (or seveal? All the boids?) for which we describe methods and more.
+"""
+A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
+for use as an exercise on refactoring.
+"""
 
-# So something like:
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import numpy as np
+import random
 
-my_boids = Boids(nr_of_boids)
+# Deliberately terrible code for teaching purposes
 
-for something in something:
-    my_boids.update(time?)
+boid_no = 100
+lower_pos_limits = np.array([100, 900])
+upper_pos_limits = np.array([200,1100])
+lower_vel_limits = np.array([0,-20])
+upper_vel_limits = np.array([10,20])
 
-This should be the body of the code. 
+
+
+def new_flock(count, lower_limits, upper_limits):
+	width=upper_limits-lower_limits
+	return lower_limits[:,np.newaxis]  + np.random.rand(2, count)*width[:,np.newaxis]
+
+
+positions=new_flock(boid_no, np.array([100, 900]), np.array([200,1100]))
+
+velocities=new_flock(boid_no, np.array([0,-20]), np.array([10,20]))
+
+#positions=new_flock(boid_no, lower_pos_limits, upper_pos_limits)
+#velocities=new_flock(boid_no, lower_vel_limits, upper_vel_limits)
+
+
+
+boid_no = 3
+
+boids_x=[random.uniform(-450,50.0) for x in range(boid_no)]
+boids_y=[random.uniform(300.0,600.0) for x in range(boid_no)]
+boid_positions = (boids_x, boids_y)
+
+boid_x_velocities=[random.uniform(0,10.0) for x in range(boid_no)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(boid_no)]
+
+old_boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+print len(old_boids)
+boids=(positions[0], positions[1], velocities[0], velocities[1])
+print len(boids)
+
+def update_boids(boids):
+	xs,ys,xvs,yvs=boids
+	# Fly towards the middle
+	for i in range(len(xs)):
+		for j in range(len(xs)):
+			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+	for i in range(len(xs)):
+		for j in range(len(xs)):
+			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+	# Fly away from nearby boids
+	for i in range(len(xs)):
+		for j in range(len(xs)):
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+				xvs[i]=xvs[i]+(xs[i]-xs[j])
+				yvs[i]=yvs[i]+(ys[i]-ys[j])
+	# Try to match speed with nearby boids
+	for i in range(len(xs)):
+		for j in range(len(xs)):
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
+				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
+				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+	# Move according to velocities
+	for i in range(len(xs)):
+		xs[i]=xs[i]+xvs[i]
+		ys[i]=ys[i]+yvs[i]
+
+
+figure=plt.figure()
+axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
+scatter=axes.scatter(boids[0],boids[1])
+
+def animate(frame):
+   update_boids(boids)
+   scatter.set_offsets(zip(boids[0],boids[1]))
+
+
+anim = animation.FuncAnimation(figure, animate,
+                               frames=50, interval=50)
+
+if __name__ == "__main__":
+    plt.show()
